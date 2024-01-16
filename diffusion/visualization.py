c01f144e5cf361d6c87bc4afb8cae6b48038307b
AugustoPeres <augustoluisperes@gmail.com>
2024-01-16 09:53:51 +0000

    Turn off axis in image plotting

--- diffusion/visualization.py
+++ diffusion/visualization.py
@@ -7,13 +7,15 @@ import matplotlib.pyplot as plt
 def plot_image(image,
                save_path=None,
                input_transform=lambda x: (x + 1) / 2,
-               black_and_white=True):
+               black_and_white=True,
+               axis='off'):
     image = input_transform(image)
     if black_and_white:
         image = image[0]
     else:
         image = jnp.transpose(image, (1, 2, 0))
     fig, ax = plt.subplots()
+    ax.axis(axis)
     if black_and_white:
         ax.imshow(image, cmap='gray')
     else:
